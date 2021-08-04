7b48221a4e3de8500574e949465a677ad5508bf3
prashanthsriram145 <prashanthsriram145@gmail.com>
2021-08-04 11:29:14 +0530

    initial commit

---
+++ src/test/java/com/springboot/mvcexample/MvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mvcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
