b2cfa38aee5d7f2ca2bc6867844168a075b19be2
prashanthsriram145 <prashanthsriram145@gmail.com>
2021-08-04 20:29:38 +0530

    Changes to use websocket

---
+++ src/main/java/com/springboot/mvcexample/common/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.springboot.mvcexample.common;
+
+import com.springboot.mvcexample.service.WebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketHandler handler;
+
+    WebSocketConfig(WebSocketHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws");
+    }
+}
